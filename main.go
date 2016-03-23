be998a4dbe251bb17a28968952d0822e97b466df
reevesj <jack.reeves@advancedcomputersoftware.com>
2016-03-23 15:57:21 +0000

    Update to channels and logic to improve performance.

--- main.go
+++ main.go
@@ -12,19 +12,21 @@ type serverResponse struct {
 	TimeDelay int32
 }
 
-var serverStatus chan int
+var serverStatusChan chan int
 var responseTimes chan time.Duration
 var tickets chan bool
 var monitorServer bool
+var serverStatus int
 
 func main() {
-	serverStatus = make(chan int, 3)
-	responseTimes = make(chan time.Duration, 3)
+	serverStatusChan = make(chan int)
+	responseTimes = make(chan time.Duration, 100)
 	tickets = make(chan bool)
 
 	monitorServer = true
 	go monitorServerStatus()
 	go generateTicket()
+	go provideServerStatus()
 
 	fmt.Printf("Listening on localhost:9002, will request data from JSON server.\n")
 	http.HandleFunc("/", returnJSON)         // set router
@@ -39,7 +41,7 @@ func main() {
 func returnJSON(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received request...")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	status := <-serverStatus
+	status := <-serverStatusChan
 	if status == 1 {
 		fmt.Println("Circuit Breaker Detected Failure, No return made.")
 		http.Error(w, "Circuit Breaker Detected Failure", http.StatusInternalServerError)
@@ -84,7 +86,7 @@ func getJSONAndRequestTime() (response *http.Response, elapsed time.Duration, er
 }
 
 func monitorServerStatus() {
-	var status = 0
+	serverStatus = 0
 	fmt.Println("Beginning JSON server monitoring.")
 	for monitorServer {
 		fmt.Println("Attempting to connect to JSON server...")
@@ -92,41 +94,33 @@ func monitorServerStatus() {
 
 		if err != nil {
 			fmt.Println("Server did not response, circuit breaker will stop connections.")
-			status = 1
+			serverStatus = 1
 		} else if responseTime <= time.Second*time.Duration(5) {
 			fmt.Println("Server responded in less than 5 seconds, circuit breaker will allow all traffic.")
-			status = 0
+			serverStatus = 0
 		} else {
 			fmt.Println("Server responded in over 5 seconds, circuit breaker will restrict traffic.")
-			status = 2
+			serverStatus = 2
 		}
 
 		start := time.Now()
 		for time.Since(start) < time.Second*time.Duration(5) {
-			var channelOpen = false
-			select {
-			case serverStatus <- status:
-				channelOpen = true
-				fmt.Println("Server status set in channel: ", status)
-			default:
-				fmt.Println("Server status already in channel.")
-			}
-
 			select {
 			case responseTime = <-responseTimes:
-				channelOpen = true
-				if status == 0 && responseTime > time.Second*time.Duration(5) {
-					status = 2
+				if serverStatus == 0 && responseTime > time.Second*time.Duration(5) {
+					serverStatus = 2
 				}
 			default:
-				fmt.Println("No response time available in channel.")
-			}
-			if !channelOpen {
-				fmt.Println("No channels open, sleeping for 1 second.")
+				fmt.Println("No response time available in channel, sleeping for 1 second.")
 				time.Sleep(time.Second * time.Duration(1))
 			}
-
 		}
+	}
+}
+
+func provideServerStatus() {
+	for monitorServer {
+		serverStatusChan <- serverStatus
 	}
 }
 
